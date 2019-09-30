ad5e5df0b9bd7f6373b4dfa0549537091eb30a32
Selvam Murugan <smuruga5@jcp.com>
2019-09-30 13:24:46 +0530

    spawned child processes and successfully sent messages

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
 
+let workers = {};
 if (cluster.isMaster) {
   masterProcess();
 } else {
@@ -7,9 +9,29 @@ if (cluster.isMaster) {
 }
 
 function masterProcess() {
-    console.log(`Master ${process.pid} is running`);
+  console.log(`Master ${process.pid} is running`);
+  let port = 3000;
+  // Fork workers. One per CPU for maximum effectiveness
+  for (var i = 0; i < Math.max(numCPUs * 4, 16); i++) {
+    port += 1;
+    const worker = cluster.fork();
+    workers[worker.process.pid] = {
+      port,
+      worker,
+      id: i + 1,
+      deathCount: 0
+    };
+    worker.send({
+      port,
+      id: i + 1
+    });
+  }
 }
 
 function childProcess() {
     console.log(`Child ${process.pid} is running`);
+    process.on("message", function(startMsg) {
+      // Spawn a worker with the given config
+      console.log(startMsg);
+    });
 }
