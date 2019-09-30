07dc3cb180d7e498386475e65d57ed5fe06288c5
Selvam Murugan <smuruga5@jcp.com>
2019-09-30 13:33:38 +0530

    eslint setup working fine and cleared all eslint issues

--- index.js
+++ index.js
@@ -1,55 +1,56 @@
-const cluster = require("cluster");
-const numCPUs = require("os").cpus().length;
-const spawn = require("child_process").spawn;
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const { spawn } = require('child_process');
 
-let workers = {};
-if (cluster.isMaster) {
-  masterProcess();
-} else {
-  childProcess();
-}
+const workers = {};
 
 function masterProcess() {
   console.log(`Master ${process.pid} is running`);
   let port = 3000;
   // Fork workers. One per CPU for maximum effectiveness
-  for (var i = 0; i < Math.max(numCPUs * 4, 16); i++) {
+  for (let i = 0; i < Math.max(numCPUs * 4, 16); i += 1) {
     port += 1;
     const worker = cluster.fork();
     workers[worker.process.pid] = {
       port,
       worker,
       id: i + 1,
-      deathCount: 0
+      deathCount: 0,
     };
     worker.send({
       port,
-      id: i + 1
+      id: i + 1,
     });
   }
 }
 
 function childProcess() {
-    console.log(`Child ${process.pid} is running`);
-    process.on("message", function(startMsg) {
+  console.log(`Child ${process.pid} is running`);
+  process.on('message', (startMsg) => {
     // Spawn a worker with the given config
-    let serverSpawnProcess = spawn('./bin/worker.mac', [
-      "-workerId",
+    const serverSpawnProcess = spawn('./bin/worker.mac', [
+      '-workerId',
       startMsg.id,
-      "-port",
-      startMsg.port
+      '-port',
+      startMsg.port,
     ]);
-    serverSpawnProcess.on("exit", function(code) {
+    serverSpawnProcess.on('exit', (code) => {
       process.exit();
     });
-    serverSpawnProcess.stdout.on("data", function(data) {
-      console.log("stdout: " + data);
+    serverSpawnProcess.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
     });
-    serverSpawnProcess.stderr.on("data", function(data) {
-      if (data.includes("started")) {
+    serverSpawnProcess.stderr.on('data', (data) => {
+      if (data.includes('started')) {
         process.send(startMsg.port);
       }
-      console.log("stdout: " + data);
+      console.log(`stdout: ${data}`);
     });
-    });
+  });
+}
+
+if (cluster.isMaster) {
+  masterProcess();
+} else {
+  childProcess();
 }
